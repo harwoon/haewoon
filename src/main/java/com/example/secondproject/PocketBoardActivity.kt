a4350a5c7a3eee8431d2c817e5f09ba464f70508
Han HaeWoon <dorothy8380@gmail.com>
2024-11-10 19:17:54 +0900

    Commit init

--- src/main/java/com/example/secondproject/PocketBoardActivity.kt
+++ src/main/java/com/example/secondproject/PocketBoardActivity.kt
@@ -1,23 +1,15 @@
 package com.example.secondproject
 
 import PocketData
-import android.app.Notification.Action
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.secondproject.databinding.ActivityPocketBoardBinding
-import com.example.secondproject.databinding.ItemPocketBinding
 
 class PocketBoardActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
